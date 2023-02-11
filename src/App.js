00135c0ddb115d362a68d3b6d9044d958fa895e3
Zane Falcao <falcaozane@gmail.com>
2023-02-11 11:14:18 +0530

    Cards on homepage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,47 @@ import ClientSignup from './components/ClientSignup';
 import ClientLogin from './components/ClientLogin';
 import WorkerSignup from './components/WorkerSignup';
 import Home from './components/Home';
+import Navbar from './components/Navbar'
+import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Box } from '@mui/system';
 import { BrowserRouter, Switch, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<><Home /></>}></Route>
+        <Route path='/' element={
+        <>
+        <Box>
+        <Navbar postion="sticky" />
+        <div className='Card-container'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </div>
+        <div className='Card-container'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </div>
+        <div className='Card-container'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </div>
+        <div className='Card-container'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </div>
+      </Box>
+      </>
+      }></Route>
         <Route path='/clientsignup' element={<><ClientSignup /></>}></Route>
         <Route path='/clientlogin' element={<><ClientLogin/></>}></Route>
         <Route path='/workersignup' element={<><WorkerSignup /></>}></Route>
---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import PlumbingIcon from '@mui/icons-material/Plumbing';
+import { width } from '@mui/system';
+import './Card.css'
+
+
+function BasicExample() {
+  return (
+    <>
+    <div className='Container'>
+    <Card style={{ width: '20rem'}}>
+      
+      <Card.Body>
+        <Card.Title>Work Title</Card.Title>
+        <Card.Text style={{paddingBottom:'50px'}}>
+          Description
+        </Card.Text>
+        
+        <Button variant="primary">Apply for Work</Button>
+        <PlumbingIcon style={{marginLeft:'50px'}} />
+      </Card.Body>
+    </Card>
+    </div>
+    
+    </>
+  );
+}
+
+export default BasicExample;
